1dd47004615297685df5793870cd6ff343dafdef
chrislatorres <me@chrislatorres.com>
2019-04-05 20:08:18 -0500

    update redirects

--- scripts/redirects.js
+++ scripts/redirects.js
@@ -23,11 +23,35 @@ hexo.extend.generator.register('community-short-url-redirects', function () {
   return expandRedirectObjs([
     ['github/', hexo.config.github.exokit.url],
     ['repo/', hexo.config.github.exokit.url],
+    ['gh/', hexo.config.github.exokit.url],
 
     ['twitter/', hexo.config.twitter.url],
 
+    ['instagram/', hexo.config.instagram.url],
+    ['insta/', hexo.config.instagram.url],
+
+    ['facebook/', hexo.config.facebook.url],
+    ['fb/', hexo.config.facebook.url],
+
+    ['youtube/', hexo.config.youtube.url],
+
+    ['twitch/', hexo.config.twitch.url],
+    ['twitchtv/', hexo.config.twitch.url],
+    ['live/', hexo.config.twitch.url],
+    ['livestream/', hexo.config.twitch.url],
+
     ['slack/', hexo.config.slack.exokit.signup_url],
-    ['chat/', hexo.config.slack.exokit.signup_url],
+
+    ['discord/', hexo.config.discord.exokit.url],
+    ['chat/', hexo.config.discord.exokit.url],
+
+    ['reddit/', hexo.config.reddit.webvr.url],
+
+    ['mailinglist/', hexo.config.mailinglist.url],
+    ['signup/', hexo.config.mailinglist.url],
+    ['mailing/', hexo.config.mailinglist.url],
+    ['maillist/', hexo.config.mailinglist.url],
+    ['newsletter/', hexo.config.mailinglist.url],
 
     ['so/', hexo.config.stack_overflow.exokit.url],
     ['stack-overflow/', hexo.config.stack_overflow.exokit.url],
